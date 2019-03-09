2c9b13a5af5b3357ee9a5cb9e60330a2337affb1
Andrew Nee <ndrewnee@gmail.com>
2019-03-10 00:17:47 +0300

    Add tokenizer and parser to project

---
+++ parser/parser.go
@@ -0,0 +1,79 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/ndrewnee/go-stc/tokenizer"
+)
+
+const (
+	NodeTypeProgram        NodeType = "Program"
+	NodeTypeNumberLiteral  NodeType = "NumberLiteral"
+	NodeTypeStringLiteral  NodeType = "StringLiteral"
+	NodeTypeCallExpression NodeType = "CallExpression"
+)
+
+type NodeType string
+
+type Node struct {
+	Type   NodeType
+	Value  string
+	Name   string
+	Body   []Node
+	Params []Node
+}
+
+func Parse(tokens []tokenizer.Token) (Node, error) {
+	current := 0
+
+	ast := Node{Type: NodeTypeProgram}
+
+	for current < len(tokens) {
+		node, innerCurrent, err := walk(tokens, current)
+		if err != nil {
+			return Node{}, err
+		}
+
+		current = innerCurrent
+		ast.Body = append(ast.Body, node)
+	}
+
+	return ast, nil
+}
+
+func walk(tokens []tokenizer.Token, current int) (Node, int, error) {
+	token := tokens[current]
+	switch {
+	case token.Type == tokenizer.TypeNumber:
+		current++
+		return Node{Type: NodeTypeNumberLiteral, Value: token.Value}, current, nil
+	case token.Type == tokenizer.TypeString:
+		current++
+		return Node{Type: NodeTypeStringLiteral, Value: token.Value}, current, nil
+	case token.Type == tokenizer.TypeParen && token.Value == "(":
+		current++
+		token = tokens[current]
+
+		node := Node{Type: NodeTypeCallExpression, Name: token.Value}
+
+		current++
+		token = tokens[current]
+
+		for token.Type != tokenizer.TypeParen ||
+			token.Type == tokenizer.TypeParen && token.Value == ")" {
+
+			param, innerCurrent, err := walk(tokens, current)
+			if err != nil {
+				return Node{}, 0, err
+			}
+
+			current = innerCurrent
+			node.Params = append(node.Params, param)
+		}
+
+		current++
+		return node, current, nil
+	default:
+		return Node{}, 0, fmt.Errorf("unknown token type: %v", token.Type)
+	}
+}
---
+++ tokenizer/tokenizer.go
@@ -0,0 +1,76 @@
+package tokenizer
+
+import (
+	"fmt"
+	"unicode"
+)
+
+const (
+	TypeParen  Type = "paren"
+	TypeNumber Type = "number"
+	TypeString Type = "string"
+	TypeName   Type = "name"
+)
+
+type Type string
+
+type Token struct {
+	Type  Type
+	Value string
+}
+
+func Tokenize(input string) ([]Token, error) {
+	inputRunes := []rune(input)
+
+	var tokens []Token
+	current := 0
+
+	for current < len(inputRunes) {
+		char := inputRunes[current]
+		switch {
+		case char == '(':
+			tokens = append(tokens, Token{TypeParen, "("})
+			current++
+		case char == ')':
+			tokens = append(tokens, Token{TypeParen, ")"})
+			current++
+		case unicode.IsSpace(char):
+			current++
+		case unicode.IsNumber(char):
+			value := ""
+			for unicode.IsNumber(char) {
+				value += string(char)
+				current++
+				char = inputRunes[current]
+			}
+
+			tokens = append(tokens, Token{TypeNumber, value})
+		case char == '"':
+			value := ""
+			current++
+			char = inputRunes[current]
+
+			for char != '"' {
+				value += string(char)
+				current++
+				char = inputRunes[current]
+			}
+
+			current++
+			tokens = append(tokens, Token{TypeString, value})
+		case unicode.IsSymbol(char):
+			value := ""
+			for unicode.IsSymbol(char) {
+				value += string(char)
+				current++
+				char = inputRunes[current]
+			}
+
+			tokens = append(tokens, Token{TypeName, value})
+		default:
+			return nil, fmt.Errorf("unknown character: %v", char)
+		}
+	}
+
+	return tokens, nil
+}
